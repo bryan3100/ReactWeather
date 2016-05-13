d30dd7764973f3a728ee531265c3c4ad08511991
Bryan Croteau <bryan@example.com>
2016-05-13 16:58:45 -0700

    Worked on the About page.

--- app/app.jsx
+++ app/app.jsx
@@ -6,9 +6,10 @@ var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
 
-// Load foundation
+// Load foundation --------------------------------------------
 require ('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
+// ------------------------------------------------------------
 
 ReactDOM.render(
   <Router history={hashHistory}>
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 
+// Express server
 var app = express();
+// Get the process env port (needed by Heroku)
 const PORT = process.env.PORT || 3000;
 
 
+// A bit of code to handle OpenWeatherMap.org http
 app.use(function (req, res, next) {
   if(req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
@@ -14,6 +17,7 @@ app.use(function (req, res, next) {
 
 app.use(express.static('public'));
 
+// Listen on PORT
 app.listen(PORT, function() {
   console.log('Express server is up on port ' + PORT);
 });
